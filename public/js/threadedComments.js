026b3eecc3e105ed591b719e46dc415e64b04dcc
unknown <evanogas@gmail.com>
2014-02-19 22:29:35 -0600

    show and hide comment reply form

---
+++ public/js/threadedComments.js
@@ -0,0 +1,16 @@
+
+var showReply = function(id) {
+	var replyLink = document.getElementById('comment-' + id + '-reply-link'),
+		replyForm = document.getElementById('comment-' + id + '-reply-form');
+
+	replyForm.classList.remove('hidden-reply');
+	replyLink.classList.add('hidden-reply');
+};
+
+var hideReply = function(id) {
+	var replyLink = document.getElementById('comment-' + id + '-reply-link'),
+		replyForm = document.getElementById('comment-' + id + '-reply-form');
+
+	replyForm.classList.add('hidden-reply');
+	replyLink.classList.remove('hidden-reply');
+};
