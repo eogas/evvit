3f39bb93553363443858a31c25f8fe23167d6635
unknown <evanogas@gmail.com>
2014-02-20 22:00:51 -0600

    implement comment reply, resolves #14

--- routes/comment.js
+++ routes/comment.js
@@ -25,4 +25,32 @@ module.exports = function(app) {
 			});
 		});
 	});
+
+
+	app.post('/comment/:id/reply', function(req, res) {
+		var commentId = req.params.id,
+			postId = req.query.post,
+			text = req.body.text;
+
+		req.models.Comment.get(commentId, function(err, comment) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+
+			req.models.Comment.create([{
+				parentComment: comment,
+				author: req.user,
+				text: text,
+				date: new Date()
+			}], function(err, items) {
+				if (err) {
+					console.log(err);
+					return;
+				}
+
+				res.redirect('/post/' + postId);
+			});
+		});
+	});
 };
