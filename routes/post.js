def65b732b4034efa7ab974675a90e6813c194ff
unknown <evanogas@gmail.com>
2014-02-22 15:35:32 -0600

    threaded comments! resolves #12

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,23 @@
 
+var util = require('../util.js');
+
+var walkComments = function(comments, cb) {
+	var head = util.head(comments),
+		tail = util.tail(comments);
+
+	if (!head) {
+		cb();
+	} else {
+		head.getChildren(function(err, subComments) {
+			// step into children
+			walkComments(subComments, function() {
+				// continue with siblings
+				walkComments(tail, cb);
+			});
+		});
+	}
+};
+
 module.exports = function(app) {
 	app.get('/post/:id', function(req, res) {
 		var postId = req.params.id;
@@ -15,9 +34,11 @@ module.exports = function(app) {
 					return;
 				}
 
-				res.render('post.html', {
-					post: post,
-					user: req.user
+				walkComments(comments, function() {
+					res.render('post.html', {
+						post: post,
+						user: req.user
+					});
 				});
 			});
 		});
