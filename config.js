7364a7550e7413c226722ca6802b20e101a2fc4a
Evan Ogas <evanogas@gmail.com>
2014-02-25 19:59:31 -0600

    fix config file reading and writing

--- config.js
+++ config.js
@@ -2,15 +2,18 @@
 var fs = require('fs'),
 	defaultConfigPath = './default_config.json',
 	configPath = './config.json',
-	configData;
+	configRaw,
+	config;
 
 try {
-	configData = fs.readFileSync(configPath);
+	configRaw = fs.readFileSync(configPath);
+	config = JSON.parse(configRaw);
 } catch (err) {
 	// config file doesn't exist, create a new config file
 	// with default settings from default_config.json
-	configData = fs.readFileSync(defaultConfigPath);
-	fs.writeFile(configPath, configData);
+	configRaw = fs.readFileSync(defaultConfigPath);
+	config = JSON.parse(configRaw);
+	fs.writeFile(configPath, JSON.stringify(config, null, 4));
 } finally {
-	module.exports = JSON.parse(configData);
+	module.exports = config;
 }
