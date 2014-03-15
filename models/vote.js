3a2ec063f897b06be0ab95526488228fab569d6e
Evan Ogas <evanogas@gmail.com>
2014-03-15 18:11:01 -0500

    create vote model

---
+++ models/vote.js
@@ -0,0 +1,22 @@
+
+module.exports = function(db) {
+	var Vote = db.define('vote', {
+		value: int
+	});
+
+	Vote.setRelations = function(relModels) {
+		Vote.hasOne('voter', relModels.User, {
+			reverse: 'votes'
+		});
+
+		Vote.hasOne('post', relModels.Post, {
+			reverse: 'votes'
+		});
+
+		Vote.hasOne('comment', relModels.Comment, {
+			reverse: 'votes'
+		});
+	};
+
+	return Vote;
+};
