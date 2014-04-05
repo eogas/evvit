40dfa8f6ee3b5e5f6abb6f93162767c2bf9f4b1e
Evan Ogas <evanogas@gmail.com>
2014-04-05 00:41:22 -0500

    update home route for sequelize

--- routes/home.js
+++ routes/home.js
@@ -3,12 +3,8 @@ var models = require('../models');
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        models.Post.find({}, ['date', 'Z'], function(err, posts) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
+        models.Post.findAll({order: 'date DESC'})
+        .success(function(posts) {
             res.render('home.html', {
                 posts: posts,
                 user: req.user
